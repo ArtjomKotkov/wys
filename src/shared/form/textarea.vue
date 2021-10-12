7c8be3b269d0574e63184d159e0bf4490df7a4db
akotkov <artjom.kotkov@yandex.ru>
2021-10-12 23:23:33 +0300

    добавил компонент текстарии

---
+++ src/shared/form/textarea.vue
@@ -0,0 +1,189 @@
+<template>
+  <div class="root-wrapper">
+    <div class="textarea" :data-style="style_" :data-is-valid="isValid">
+      <span class="title" v-if="label">{{label}}</span>
+      <textarea
+          :value="modelValue"
+          @input="$emit('update:modelValue', $event.target.value)"
+          :data-type="type"
+          :placeholder="placeholder"
+      >
+      </textarea>
+      <div class="line" v-if="style_ === 'monolith'"></div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+
+.root-wrapper {
+  padding: 0 0;
+}
+
+.textarea[data-style="monolith"] {
+  margin-top: 21px;
+  width: 400px;
+  background-color: var(--medium-gray);
+  outline: none;
+  border: none;
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  position: relative;
+
+  .title {
+    height: 20px;
+
+    color: var(--light-gray);
+
+    position: absolute;
+    top: -21px;
+
+    line-height: 20px;
+    font-size: 16px;
+    font-weight: 400;
+
+    padding: 0 10px;
+  }
+
+  textarea {
+    font-size: 16px;
+    width: 100%;
+
+    flex-grow: 1;
+    outline: none;
+    border: none;
+
+    background-color: inherit;
+    height: 100%;
+
+    line-height: 24px;
+    padding: 20px;
+
+    overflow: hidden;
+
+    min-height: 24px;
+
+    &::placeholder {
+      color: var(--light-gray);
+      font-size: 18px;
+      line-height: 24px;
+    }
+  }
+
+}
+
+.textarea[data-style="rounded"] {
+  outline: 2px solid #785FF7;
+  background-color: var(--dark-gray);
+
+  position: relative;
+
+  font-size: 30px;
+  line-height: 60px;
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  border-radius: 15px;
+
+  padding: 5px;
+
+  &:focus {
+    outline: 2px solid #785FF7;
+    text-align: center;
+  }
+
+  &[data-is-valid=false] {
+    outline: 2px solid var(--invalid-red);
+  }
+
+  textarea {
+    font-size: 30px;
+    width: 100%;
+
+    flex-grow: 1;
+    outline: none;
+    border: none;
+
+    background-color: inherit;
+    height: 100%;
+
+    line-height: 60px;
+    padding: 0 20px;
+
+    overflow: hidden;
+
+    &::placeholder {
+      color: var(--light-gray);
+      font-size: 18px;
+      line-height: 60px;
+    }
+  }
+
+  .title {
+    height: 20px;
+    background-color: var(--dark-gray);
+
+    color: var(--light-gray);
+
+    position: absolute;
+    left: 20px;
+    top: -10px;
+
+    line-height: 20px;
+    font-size: 16px;
+    font-weight: 400;
+
+    padding: 0 10px;
+  }
+}
+
+textarea {
+  color: white;
+  font-family: 'Roboto', sans-serif;
+  font-weight: 400;
+}
+
+.line {
+  position: absolute;
+
+  bottom: 0;
+  width: calc(100% - 40px);
+  margin-left: 20px;
+  height: 2px;
+  background-color: #785FF7;;
+}
+
+.input[data-is-valid=false] {
+  textarea {
+    color: var(--invalid-red);
+  }
+
+  .line {
+    background-color: var(--invalid-red);
+  }
+
+  .toggle-showing {
+    background-color: var(--invalid-red);
+  }
+}
+</style>
+
+<script lang="ts">
+import {Options, Vue} from 'vue-class-component';
+import {Prop} from 'vue-property-decorator';
+
+
+@Options({})
+export default class TextareaComponent extends Vue {
+  @Prop(String) modelValue!: string;
+  @Prop(String) style_: 'rounded' | 'monolith' = 'rounded';
+  @Prop(String) label = '';
+  @Prop(String) placeholder?: string;
+  @Prop(Boolean) isValid = true;
+}
+</script>
--- src/views/main-component/report-redactor-component/report-redactor.vue
+++ src/views/main-component/report-redactor-component/report-redactor.vue
@@ -1,18 +1,53 @@
 <template>
-  <div>
+  <div class="report-redactor-wrapper">
 
   </div>
 </template>
 
 <style lang="scss">
+  @media (max-width: 1366px) {
+    .report-redactor-wrapper {
+      overflow: visible !important;
+    }
+  }
 
+  .report-redactor-wrapper {
+    flex-grow: 1;
+
+    background-color: var(--dark-gray);
+
+    overflow: auto;
+    height: 100%;
+
+    &::-webkit-scrollbar {
+      width: 8px;
+    }
+
+    &::-webkit-scrollbar-track {
+      background: var(--light-gray);
+    }
+
+    &::-webkit-scrollbar-thumb {
+      background-color: var(--main-color);
+    }
+
+  }
+
+  .report-redactor-wrapper {
+    padding: 20px;
+  }
 </style>
 
 <script lang="ts">
 import {Options, Vue} from 'vue-class-component';
+import TextareaComponent from "@/shared/form/textarea.vue";
 
 
-@Options({})
+@Options({
+  components: {
+    TextareaComponent,
+  }
+})
 export default class ReportRedactorComponent extends Vue {
 }
 </script>
